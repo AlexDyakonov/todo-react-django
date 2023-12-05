54b7d830b289522788783387329cad959aec38d6
AlexDyakonov <shram.monolit@mail.ru>
2023-12-06 01:07:41 +0300

    Added tailwindcss and started fronend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// import Table from './components/Table'
+// import TodoForm from './components/TodoForm'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-indigo-100'>
+      <nav className='pt-8' >
+        <h1 className=' text-5xl text-center pb-8'>To Do List </h1>
+      </nav>    
+
+      {/* <Table />
+      <TodoForm /> */}
+
+    </div>
   )
 }
 
