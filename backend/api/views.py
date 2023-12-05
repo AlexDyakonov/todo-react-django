f38407d8bd04fc9661fe8bc15098963c3fea5302
AlexDyakonov <shram.monolit@mail.ru>
2023-12-06 00:47:35 +0300

    Added base model and api router

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.http import Http404
+from . import serializers
+from . import models
 
 # Create your views here.
+class ToDoViewSet(viewsets.ModelViewSet):
+    queryset = models.Todo.objects.all()
+    serializer_class = serializers.TodoSerializer
