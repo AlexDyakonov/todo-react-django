d9026f98776b952a910367fc3d3ff87c3a07437b
AlexDyakonov <shram.monolit@mail.ru>
2023-12-11 19:57:55 +0300

    New methods from rest api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from .views import TodoList, TodoDetail
 
 
-router = routers.DefaultRouter()
-router.register('todo', viewset=views.ToDoViewSet, basename='todo')
+# router = routers.DefaultRouter()
+# router.register('todo', viewset=views.ToDoViewSet, basename='todo')
 
 urlpatterns = [
+    path('todo/', TodoList.as_view(), name='todo-list'),
+    path('todo/<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
 ]
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
